5a1eaa92e162c03ff80ce225e6647725729076f5
albert ou <jieua.ou@mail.com>
2023-10-14 12:24:59 +0800

    ENABLE_HELLO_ERROR

--- hello-app/src/hello.js
+++ hello-app/src/hello.js
@@ -1,6 +1,27 @@
 const Router = require("koa-router");
 const router = new Router();
 
+const stringToBoolean = (stringValue) => {
+    switch(stringValue?.toLowerCase()?.trim()){
+        case "true": 
+        case "yes": 
+        case "1": 
+          return true;
+
+        case "false": 
+        case "no": 
+        case "0": 
+          return false;
+
+        default: 
+          return true;  // default to true
+    }
+}
+
+console.log(`ENABLE_HELLO_ERROR - config ${process.env.ENABLE_HELLO_ERROR}`);
+
+const enabled = stringToBoolean(process.env.ENABLE_HELLO_ERROR);
+console.log(`ENABLE_HELLO_ERROR - eval ${enabled}`);
 
 router.get("/api/hello", async (ctx) => {
     console.log("Hello 1 ... called " + new Date());
@@ -28,14 +49,21 @@ router.get("/api/hello/error/:code/:id", async (ctx) => {
     const id = ctx.params.id;
     console.log(`Hello error ${id} ... called at ${timestamp} ` );
     const code = parseInt(ctx.params.code) | 500;  // error code 5xx, 4xx
+   
+    if(!enabled){
+        ctx.status = 200;
+        ctx.body = `(${id}) something good ${code} at ${timestamp} \n`;
+        console.log(`Hello non-error ${id}  ... done`);
+        return;
+    }
+
     await new Promise((resolve) => {
         setTimeout(() => {
             ctx.status = code;
-            
             ctx.body = `(${id}) something wrong ${code} at ${timestamp} \n`;
             console.log(`Hello error ${id}  ... done`);
             resolve();
-        }, 500);
+        }, 100);
     })
 });
 
